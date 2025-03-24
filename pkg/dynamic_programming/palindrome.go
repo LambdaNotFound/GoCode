cedc9175403bc66f5bce5b94f666740890223dee
LambdaNotFound <yiqyudev@gmail.com>
2025-03-23 23:05:35 -0700

    DP palindrome

--- pkg/dynamic_programming/palindrome.go
+++ pkg/dynamic_programming/palindrome.go
@@ -5,9 +5,9 @@ package palindrome
  *
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
  *
- * i. DynamicProgramming <= Time: O(n^2), Space: O(n^2) -> O(n)
- *    dp[i][j] marks if string from i to j is palindrome
- *    dp[i][j] == true if (s[i] == s[j] && (j - i < 2 || table[i + 1][j - 1))
+ * DynamicProgramming <= Time: O(n^2), Space: O(n^2)
+ *     dp[i][j] marks if string from i to j is palindrome
+ *     dp[i][j] == true if (s[i] == s[j] && (j - i + 1 <= 2 || table[i + 1][j - 1))
  */
 func longestPalindrome(s string) string {
     start, length, size := 0, 1, len(s)
@@ -18,9 +18,33 @@ func longestPalindrome(s string) string {
 
     for j := 0; j < size; j++ {
         for i := 0; i <= j; i++ {
-            dp[i][j] = (s[i] == s[j]) && (j-i <= 2 || dp[i+1][j-1])
+            dp[i][j] = (s[i] == s[j]) && (j-i+1 <= 2 || dp[i+1][j-1])
 
             if dp[i][j] && (length < j-i+1) {
+                length = j - i + 1
+                start = i
+            }
+        }
+    }
+
+    return s[start : start+length]
+}
+
+/**
+ * Space optimize to O(n)
+ *    ->
+ *  |           [ i ][ j ]
+ *  v [i+1][j-1]
+ */
+func longestPalindrome_opt_space(s string) string {
+    start, length, size := 0, 1, len(s)
+    dp := make([]bool, size)
+
+    for j := 0; j < size; j++ {
+        for i := 0; i <= j; i++ {
+            dp[i] = (s[i] == s[j]) && (j-i+1 <= 2 || dp[i+1])
+
+            if dp[i] && (length < j-i+1) {
                 length = j - i + 1
                 start = i
             }
--- pkg/dynamic_programming/palindrome_test.go
+++ pkg/dynamic_programming/palindrome_test.go
@@ -17,11 +17,19 @@ func Test_longestPalindrome(t *testing.T) {
             "babad",
             "bab",
         },
+        {
+            "case 2",
+            "cbbd",
+            "bb",
+        },
     }
 
     for _, tc := range testCases {
         t.Run(tc.name, func(t *testing.T) {
             result := longestPalindrome(tc.input)
+            assert.Equal(t, tc.expected, result)
+
+            result = longestPalindrome_opt_space(tc.input)
             assert.Equal(t, tc.expected, result)
         })
     }
