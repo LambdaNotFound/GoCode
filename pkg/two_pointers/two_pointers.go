dff124d7064225346569d346f871c8068653806c
LambdaNotFound <yiqyudev@gmail.com>
2025-03-23 18:11:16 -0700

    fix typo

--- pkg/two_pointers/two_pointers.go
+++ pkg/two_pointers/two_pointers.go
@@ -8,7 +8,6 @@ func twoSum(numbers []int, target int) []int {
         sum := numbers[left] + numbers[right]
 
         if sum < target {
-            a
             left += 1
         } else if sum == target {
             return []int{left + 1, right + 1}
