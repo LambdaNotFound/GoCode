b315d96da7e8d15db8b19c915a97e1a82607d8d5
LambdaNotFound <yiqyudev@gmail.com>
2025-03-25 00:21:20 -0700

    add UT

---
+++ pkg/two_pointers/sliding_window_test.go
@@ -0,0 +1,31 @@
+package two_pointers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+    testCases := []struct {
+        name     string
+        str      string
+        expected int
+    }{
+        {
+            "case 1",
+            "abcabcbb",
+            3,
+        },
+    }
+
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            result := lengthOfLongestSubstring(tc.str)
+            assert.Equal(t, tc.expected, result)
+
+            result = lengthOfLongestSubstring_optimized(tc.str)
+            assert.Equal(t, tc.expected, result)
+        })
+    }
+}
