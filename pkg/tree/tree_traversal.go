32488026f723c525d39ad08ca31f2ce3a3b82185
LambdaNotFound <yiqyudev@gmail.com>
2025-03-26 22:33:24 -0700

    postorder traversal

--- pkg/tree/tree_traversal.go
+++ pkg/tree/tree_traversal.go
@@ -3,6 +3,27 @@ package tree
 import . "gocode/types"
 
 /**
+ * 145. Binary Tree Postorder Traversal
+ */
+func postorderTraversal(root *TreeNode) []int {
+    res, stack := []int{}, []*TreeNode{}
+    p := root
+    for p != nil || len(stack) != 0 {
+        if p == nil {
+            p = stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            p = p.Left
+        } else {
+            res = append([]int{p.Val}, res...) // 1 Stack: [L, R, root]
+
+            stack = append(stack, p)
+            p = p.Right
+        }
+    }
+    return res
+}
+
+/**
  * 144. Binary Tree Preorder Traversal
  */
 func preorderTraversal(root *TreeNode) []int {
@@ -12,12 +33,12 @@ func preorderTraversal(root *TreeNode) []int {
         if p == nil {
             p = stack[len(stack)-1]
             stack = stack[:len(stack)-1]
-            p = p.Right
+            p = p.Right // push right child if no left child
         } else {
             res = append(res, p.Val)
 
             stack = append(stack, p)
-            p = p.Left
+            p = p.Left // push left child to stack first
         }
     }
     return res
@@ -36,10 +57,10 @@ func inorderTraversal(root *TreeNode) []int {
 
             res = append(res, p.Val)
 
-            p = p.Right // right child at last
+            p = p.Right // push right child if no left child
         } else {
             stack = append(stack, p)
-            p = p.Left
+            p = p.Left // push left child to stack first
         }
     }
     return res
