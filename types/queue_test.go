53f48244cdeefb885635078943f7a0f3775454cb
LambdaNotFound <yiqyudev@gmail.com>
2025-03-28 20:55:09 -0700

    queue UT

---
+++ types/queue_test.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Queue(t *testing.T) {
+	queue := Queue[int]{}
+	isEmpty := queue.IsEmpty()
+	assert.Equal(t, true, isEmpty)
+
+	queue.Enqueue(1)
+	isEmpty = queue.IsEmpty()
+	assert.Equal(t, false, isEmpty)
+
+	queue.Enqueue(2)
+	queue.Enqueue(3)
+	value, ok := queue.Dequeue()
+	assert.Equal(t, 1, value)
+	assert.Equal(t, true, ok)
+
+	queue.Dequeue()
+	value, ok = queue.Dequeue()
+	assert.Equal(t, 3, value)
+	assert.Equal(t, true, ok)
+
+	value, ok = queue.Dequeue()
+	assert.Equal(t, 0, value)
+	assert.Equal(t, false, ok)
+}
