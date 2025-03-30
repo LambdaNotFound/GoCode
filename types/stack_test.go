cac651b40f00006cf11f3663f12cfb9ff32d6f06
LambdaNotFound <yiqyudev@gmail.com>
2025-03-29 20:38:06 -0700

    update tests

---
+++ types/stack_test.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Stack(t *testing.T) {
+    stack := Stack[int]{}
+    isEmpty := stack.IsEmpty()
+    assert.Equal(t, true, isEmpty)
+
+    stack.Push(1)
+    isEmpty = stack.IsEmpty()
+    assert.Equal(t, false, isEmpty)
+
+    stack.Push(2)
+    stack.Push(3)
+    value, ok := stack.Pop()
+    assert.Equal(t, 3, value)
+    assert.Equal(t, true, ok)
+
+    stack.Pop()
+    value, ok = stack.Pop()
+    assert.Equal(t, 1, value)
+    assert.Equal(t, true, ok)
+
+    value, ok = stack.Pop()
+    assert.Equal(t, 0, value)
+    assert.Equal(t, false, ok)
+}
