ab59cee66829c0d2107ffcbb9be9d43866823339
LambdaNotFound <yiqyudev@gmail.com>
2025-04-16 13:35:32 -0700

    UT for LRU cache

--- pkg/api_design/lru_cache.go
+++ pkg/api_design/lru_cache.go
@@ -1,5 +1,9 @@
 package apidesign
 
+/**
+ * 146. LRU Cache
+ */
+
 type DoublyLinkedList struct {
     key  int
     val  int
@@ -27,43 +31,43 @@ func Constructor(capacity int) LRUCache {
     }
 }
 
-func (this *LRUCache) remove(node *DoublyLinkedList) {
+func (l *LRUCache) remove(node *DoublyLinkedList) {
     prev := node.prev
     next := node.next
     prev.next = next
     next.prev = prev
 }
 
-func (this *LRUCache) insert(node *DoublyLinkedList) {
-    headNext := this.head.next
-    this.head.next = node
-    node.prev = this.head
+func (l *LRUCache) insert(node *DoublyLinkedList) {
+    headNext := l.head.next
+    l.head.next = node
+    node.prev = l.head
     node.next = headNext
     headNext.prev = node
 }
 
-func (this *LRUCache) Get(key int) int {
-    if node, ok := this.cache[key]; ok {
-        this.remove(node)
-        this.insert(node)
+func (l *LRUCache) Get(key int) int {
+    if node, ok := l.cache[key]; ok {
+        l.remove(node)
+        l.insert(node)
         return node.val
     }
     return -1
 }
 
-func (this *LRUCache) Put(key int, value int) {
-    if node, ok := this.cache[key]; ok {
-        this.remove(node)
+func (l *LRUCache) Put(key int, value int) {
+    if node, ok := l.cache[key]; ok {
+        l.remove(node)
         node.val = value
-        this.insert(node)
+        l.insert(node)
     } else {
         newNode := &DoublyLinkedList{key: key, val: value}
-        this.cache[key] = newNode
-        this.insert(newNode)
-        if len(this.cache) > this.capacity {
-            lru := this.tail.prev
-            this.remove(lru)
-            delete(this.cache, lru.key)
+        l.cache[key] = newNode
+        l.insert(newNode)
+        if len(l.cache) > l.capacity {
+            lru := l.tail.prev
+            l.remove(lru)
+            delete(l.cache, lru.key)
         }
     }
 }
--- pkg/api_design/lru_cache_test.go
+++ pkg/api_design/lru_cache_test.go
@@ -1,2 +1,29 @@
 package apidesign
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LRUCache(t *testing.T) {
+    cache := Constructor(3)
+
+    cache.Put(1, 1)
+    cache.Put(2, 2)
+    value := cache.Get(1)    
+    assert.Equal(t, 1, value)
+
+    cache.Put(3, 3)
+    value = cache.Get(2)
+    assert.Equal(t, 2, value)
+
+    cache.Put(4, 4)
+    value = cache.Get(1)
+    assert.Equal(t, -1, value)
+    value = cache.Get(4)
+    assert.Equal(t, 4, value)
+
+    value = cache.Get(3)
+    assert.Equal(t, 3, value)
+}
