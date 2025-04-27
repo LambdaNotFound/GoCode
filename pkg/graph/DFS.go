a3377840067f69074fc67be99c3b55911591ddac
LambdaNotFound <yiqyudev@gmail.com>
2025-04-26 19:21:13 -0700

    refacotoring

--- pkg/graph/DFS.go
+++ pkg/graph/DFS.go
@@ -8,23 +8,23 @@ import (
  * 543. Diameter of Binary Tree
  */
 func diameterOfBinaryTree(root *TreeNode) int {
-    max := 0
+    res := 0
+    max := func(a, b int) int {
+        if a > b {
+            return a
+        }
+        return b
+    }
+
     var dfs func(node *TreeNode) int
     dfs = func(node *TreeNode) int {
         if node == nil {
             return 0
         }
         left, right := dfs(node.Left), dfs(node.Right)
-        max = Max(max, left+right)
-        return 1 + Max(left, right)
+        res = max(res, left+right)
+        return 1 + max(left, right)
     }
     dfs(root)
-    return max
-}
-
-func Max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+    return res
 }
