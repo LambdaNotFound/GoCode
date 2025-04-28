572ca5fce3f2013b1bf3797cae3d8128c6dea541
LambdaNotFound <yiqyudev@gmail.com>
2025-04-27 23:41:38 -0700

    Search in Rotated Sorted Array

--- pkg/binary_search/binary_search.go
+++ pkg/binary_search/binary_search.go
@@ -1,5 +1,33 @@
 package binarysearch
 
+/**
+ * 33. Search in Rotated Sorted Array
+ */
+func search(nums []int, target int) int {
+    left, right := 0, len(nums)-1
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[mid] < nums[right] {
+            if nums[mid] < target && target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else {
+            if nums[left] <= target && target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+    }
+    return -1
+}
+
 /*
  * 34. Find First and Last Position of Element in Sorted Array
  */
@@ -55,7 +83,7 @@ func firstBadVersion(n int) int {
 /**
  * 704. Binary Search
  */
-func search(nums []int, target int) int {
+func binarySearch(nums []int, target int) int {
     left, right := 0, len(nums)
     for left < right {
         mid := left + (right-left)/2
