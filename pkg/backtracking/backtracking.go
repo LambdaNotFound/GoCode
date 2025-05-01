d792e0b1b6576acdad1767bb054a6658c3956471
LambdaNotFound <yiqyudev@gmail.com>
2025-04-30 21:14:13 -0700

    refactoring

--- pkg/backtracking/backtracking.go
+++ pkg/backtracking/backtracking.go
@@ -18,9 +18,7 @@ func letterCombinations(digits string) []string {
     return res
 }
 
-// in C++
 // void letterCombinationsHelper(string digits, string& out, vector<string>& result, vector<string>& mapping) {
-// golang pass by value
 func letterCombinationsHelper(digits string, out string, res *[]string, dict []string) {
     if len(digits) == 0 {
         *res = append(*res, out)
@@ -33,6 +31,35 @@ func letterCombinationsHelper(digits string, out string, res *[]string, dict []s
             out = out[:len(out)-1]
         }
     }
+}
+
+func letterCombinationsBacktrack(digits string) []string {
+    if len(digits) == 0 {
+        return []string{}
+    }
+
+    dict := []string{"", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz",
+    }
+    out := ""
+    res := []string{}
+    var backtrack func(string, string)
+    backtrack = func(digits string, out string) {
+        if len(digits) == 0 {
+            res = append(res, out)
+        } else {
+            keys := dict[digits[0]-'0']
+            next := digits[1:]
+            for i := 0; i < len(keys); i++ {
+                out = out + string(keys[i])
+                backtrack(next, out)
+                out = out[:len(out)-1]
+            }
+        }
+    }
+    backtrack(digits, out)
+
+    return res
 }
 
 /**
