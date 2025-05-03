860338c7ad8f0d5f65fb6e61f6fac2af64aa0783
LambdaNotFound <yiqyudev@gmail.com>
2025-05-02 17:32:05 -0700

    backtrack

--- pkg/backtracking/backtracking.go
+++ pkg/backtracking/backtracking.go
@@ -12,13 +12,41 @@ func letterCombinations(digits string) []string {
         "jkl", "mno", "pqrs", "tuv", "wxyz",
     }
     out := ""
+    res := []string{}
+
+    var backtrack func(string, string)
+    backtrack = func(target string, out string) {
+        if len(target) == 0 {
+            res = append(res, out)
+        } else {
+            keys := dict[target[0]-'0']
+            next := target[1:]
+            for i := 0; i < len(keys); i++ {
+                out = out + string(keys[i])
+                backtrack(next, out)
+                out = out[:len(out)-1]
+            }
+        }
+    }
+    backtrack(digits, out)
+
+    return res
+}
+
+func letterCombinationsBacktrack(digits string) []string {
+    if len(digits) == 0 {
+        return []string{}
+    }
+
+    dict := []string{"", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz",
+    }
+    out := ""
     res := []string{} // pass in reference
     letterCombinationsHelper(digits, out, &res, dict)
 
     return res
 }
-
-// void letterCombinationsHelper(string digits, string& out, vector<string>& result, vector<string>& mapping) {
 func letterCombinationsHelper(digits string, out string, res *[]string, dict []string) {
     if len(digits) == 0 {
         *res = append(*res, out)
@@ -33,43 +61,18 @@ func letterCombinationsHelper(digits string, out string, res *[]string, dict []s
     }
 }
 
-func letterCombinationsBacktrack(digits string) []string {
-    if len(digits) == 0 {
-        return []string{}
-    }
-
-    dict := []string{"", "", "abc", "def", "ghi",
-        "jkl", "mno", "pqrs", "tuv", "wxyz",
-    }
-    out := ""
-    res := []string{}
-    var backtrack func(string, string)
-    backtrack = func(digits string, out string) {
-        if len(digits) == 0 {
-            res = append(res, out)
-        } else {
-            keys := dict[digits[0]-'0']
-            next := digits[1:]
-            for i := 0; i < len(keys); i++ {
-                out = out + string(keys[i])
-                backtrack(next, out)
-                out = out[:len(out)-1]
-            }
-        }
-    }
-    backtrack(digits, out)
-
-    return res
-}
-
 /**
  * 39. Combination Sum
+ *
+ * Given an array of distinct integers candidates and a target integer target, 
+ * return a list of all unique combinations of candidates where the chosen numbers sum to target.
+ *
  */
 func combinationSum(candidates []int, target int) [][]int {
     var result [][]int
-    var backtrack func([]int, int, []int)
+    var backtrack func(int, []int, []int)
 
-    backtrack = func(candidates []int, target int, selected []int) {
+    backtrack = func(target int, candidates []int, selected []int) {
         if target == 0 {
             result = append(result, append([]int{}, selected...))
             return
@@ -77,13 +80,13 @@ func combinationSum(candidates []int, target int) [][]int {
         for i, val := range candidates {
             if val <= target {
                 newTarget := target - val
+                newCandidates := append([]int{}, candidates[i:]...)
                 selected := append([]int{}, selected...)
                 selected = append(selected, val)
-                newCandidates := append([]int{}, candidates[i:]...)
-                backtrack(newCandidates, newTarget, selected)
+                backtrack(newTarget, newCandidates, selected)
             }
         }
     }
-    backtrack(candidates, target, []int{})
+    backtrack(target, candidates, []int{})
     return result
 }
--- pkg/backtracking/backtracking_test.go
+++ pkg/backtracking/backtracking_test.go
@@ -1,6 +1,7 @@
 package backtracking
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,6 +32,48 @@ func Test_letterCombinations(t *testing.T) {
 
             result = letterCombinationsBacktrack(tc.digits)
             assert.Equal(t, tc.expected, result)
+        })
+    }
+}
+
+func Test_combinationSum(t *testing.T) {
+    testCases := []struct {
+        name       string
+        candidates []int
+        target     int
+        expected   [][]int
+    }{
+        {
+            "case 1",
+            []int{2, 3, 6, 7},
+            7,
+            [][]int{
+                {2, 2, 3},
+                {7},
+            },
+        },
+        {
+            "case 2",
+            []int{2, 3, 5},
+            7,
+            [][]int{
+                {2, 2, 2, 2},
+                {2, 3, 3},
+                {3, 5},
+            },
+        },
+        {
+            "case 3",
+            []int{2},
+            1,
+            [][]int{},
+        },
+    }
+
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            result := combinationSum(tc.candidates, tc.target)
+            reflect.DeepEqual(tc.expected, result)
         })
     }
 }
