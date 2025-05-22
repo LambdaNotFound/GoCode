3ff10c8703171dd1bdc7dc7671dd877b8b10935e
LambdaNotFound <yiqyudev@gmail.com>
2025-05-21 20:39:42 -0700

    Number of Islands

--- pkg/graph/DFS.go
+++ pkg/graph/DFS.go
@@ -28,3 +28,45 @@ func diameterOfBinaryTree(root *TreeNode) int {
     dfs(root)
     return res
 }
+
+/**
+ * 200. Number of Islands
+ *
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+ * return the number of islands.
+ *
+ * DFS: Time: O(m x n), Space: O(m x n)
+ */
+func numIslands(grid [][]byte) int {
+    var dfsHelper func(int, int)
+    dfsHelper = func(i, j int) {
+        if grid[i][j] != '1' {
+            return
+        }
+        grid[i][j] = '2'
+
+        if i != 0 {
+            dfsHelper(i-1, j)
+        }
+        if i != len(grid)-1 {
+            dfsHelper(i+1, j)
+        }
+        if j != 0 {
+            dfsHelper(i, j-1)
+        }
+        if j != len(grid[0])-1 {
+            dfsHelper(i, j+1)
+        }
+    }
+
+    res := 0
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[0]); j++ {
+            if grid[i][j] == '1' {
+                res++
+                dfsHelper(i, j)
+            }
+        }
+    }
+    return res
+}
