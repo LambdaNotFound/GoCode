bb9cb9de76f10fa1e1ebd5ebb0434ef0aa9abd06
LambdaNotFound <yiqyudev@gmail.com>
2025-06-02 00:58:56 -0700

    Word Search

---
+++ pkg/backtracking/word_search.go
@@ -0,0 +1,39 @@
+package backtracking
+
+/**
+ * 79. Word Search
+ *
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ *
+ * Time: O(mn x 4^len) Space: O(L)
+ */
+func exist(board [][]byte, word string) bool {
+    var dfs func(int, int, int) bool
+    dfs = func(i, j, idx int) bool {
+        if idx == len(word) {
+            return true
+        }
+
+        if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) || board[i][j] != word[idx] {
+            return false
+        }
+
+        temp := board[i][j]
+        board[i][j] = '*'
+        found := dfs(i+1, j, idx+1) ||
+            dfs(i-1, j, idx+1) ||
+            dfs(i, j+1, idx+1) ||
+            dfs(i, j-1, idx+1)
+        board[i][j] = temp
+        return found
+    }
+
+    for i := 0; i < len(board); i++ {
+        for j := 0; j < len(board[0]); j++ {
+            if board[i][j] == word[0] && dfs(i, j, 0) {
+                return true
+            }
+        }
+    }
+    return false
+}
