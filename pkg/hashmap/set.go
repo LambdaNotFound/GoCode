f10cb3560740c547cc20917c8fa7f809635a915b
LambdaNotFound <yiqyudev@gmail.com>
2025-06-06 20:01:08 -0700

    Contains Duplicate

---
+++ pkg/hashmap/set.go
@@ -0,0 +1,15 @@
+package hashmap
+
+/**
+ * 217. Contains Duplicate
+ */
+func containsDuplicate(nums []int) bool {
+    set := make(map[int]struct{})
+    for _, num := range nums {
+        if _, hasNum := set[num]; hasNum {
+            return true
+        }
+        set[num] = struct{}{}
+    }
+    return false
+}
