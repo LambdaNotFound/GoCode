aea348fe667fbc9495ae3c571909dce0abbd6cb2
LambdaNotFound <yiqyudev@gmail.com>
2025-06-09 01:01:23 -0700

    Binary Tree Level Order Traversal

--- pkg/graph/BFS.go
+++ pkg/graph/BFS.go
@@ -172,3 +172,33 @@ func rightSideView(root *TreeNode) []int {
     }
     return res
 }
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ */
+func levelOrder(root *TreeNode) [][]int {
+    if root == nil {
+        return [][]int{}
+    }
+
+    var res [][]int
+    queue := []*TreeNode{root}
+    for len(queue) > 0 {
+        qlen := len(queue)
+        var level []int
+        for i := 0; i < qlen; i++ {
+            node := queue[0]
+            queue = queue[1:]
+
+            level = append(level, node.Val)
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+        res = append(res, level)
+    }
+    return res
+}
