4f3ff8152c4c0ff9535ae6b9415fcaec2c1c4f0b
LambdaNotFound <yiqyudev@gmail.com>
2025-06-12 23:29:52 -0700

    Product of Array Except Self

--- pkg/math/prefix_sum.go
+++ pkg/math/prefix_sum.go
@@ -27,3 +27,21 @@ func productExceptSelf(nums []int) []int {
     }
     return ans
 }
+
+func productExceptSelfWithMultiplier(nums []int) []int {
+    res := make([]int, len(nums))
+
+    multiplier := 1
+    for i := 0; i < len(nums); i++ {
+        res[i] = multiplier
+        multiplier *= nums[i]
+    }
+
+    multiplier = 1
+    for i := len(res) - 1; i >= 0; i-- {
+        res[i] *= multiplier
+        multiplier *= nums[i]
+    }
+
+    return res
+}
