84a76dcb98afdaf81edff88a66bc2e088e3877ea
LambdaNotFound <yiqyudev@gmail.com>
2025-06-18 20:37:58 -0700

    unit test

---
+++ pkg/graph/word_ladder_test.go
@@ -0,0 +1,39 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ladderLength(t *testing.T) {
+    testCases := []struct {
+        name     string
+        beginWord string
+        endWord string
+        wordList    []string
+        expected int
+    }{
+        {
+            "case 1",
+            "hit",
+            "cog",
+            []string{"hot","dot","dog","lot","log","cog"},
+            5,
+        },
+        {
+            "case 1",
+            "hit",
+            "cog",
+            []string{"hot","dot","dog","lot","log"},
+            0,
+        },
+    }
+
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            result := ladderLength(tc.beginWord, tc.endWord, tc.wordList)
+            assert.Equal(t, tc.expected, result)
+        })
+    }
+}
