96d23968f68cc9a25aa5c3664387ef6726525c69
LambdaNotFound <yiqyudev@gmail.com>
2025-07-09 00:02:41 -0700

    canPartition

--- pkg/dynamic_programming/knapsack.go
+++ pkg/dynamic_programming/knapsack.go
@@ -38,22 +38,21 @@ func maxSubArray(nums []int) int {
  *     dp[i] == true if dp[target - i] is true
  */
 func canPartition(nums []int) bool {
+    // if total sum is odd, can't partition into equal subsets
     totalSum := 0
     for _, num := range nums {
         totalSum += num
     }
-
-    // If total sum is odd, can't partition into equal subsets
     if totalSum%2 != 0 {
         return false
     }
 
     target := totalSum / 2
     dp := make([]bool, target+1)
-    dp[0] = true // Base case: sum 0 can always be achieved
+    dp[0] = true // base case: sum 0 can always be achieved
 
     for _, num := range nums {
-        // Iterate backwards to avoid overwriting values we need to check
+        // iterate backwards to avoid overwriting values we need to check
         for j := target; j >= num; j-- {
             if dp[j-num] {
                 dp[j] = true
