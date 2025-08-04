aba7ebe5a0ccf67401fde356654d01ffe3c44e25
LambdaNotFound <yiqyudev@gmail.com>
2025-08-04 00:48:01 -0700

    canCompleteCircuit

--- pkg/math/greedy.go
+++ pkg/math/greedy.go
@@ -1,6 +1,30 @@
 package math
 
 /**
+ * 134. Gas Station
+ * Given two integer arrays gas and cost, return the starting gas station's index
+ * if you can travel around the circuit once in the clockwise direction,
+ * otherwise return -1
+ */
+func canCompleteCircuit(gas []int, cost []int) int {
+    n := len(gas)
+    fuelLeft, globalFuelLeft, start := 0, 0, 0
+    for i := 0; i < n; i++ {
+        globalFuelLeft += gas[i] - cost[i]
+        fuelLeft += gas[i] - cost[i]
+        if fuelLeft < 0 {
+            start = i + 1
+            fuelLeft = 0
+        }
+    }
+
+    if globalFuelLeft < 0 {
+        return -1
+    }
+    return start
+}
+
+/**
  * 621. Task Scheduler
  *
  * but there's a constraint: there has to be a gap of at least n intervals between two tasks with the same label.
