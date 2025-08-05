c2033c668accba0492967847d6f5b4d2642a74a6
LambdaNotFound <yiqyudev@gmail.com>
2025-08-05 00:11:42 -0700

    Jump Game

--- pkg/math/greedy.go
+++ pkg/math/greedy.go
@@ -1,6 +1,54 @@
 package math
 
 /**
+ * Math/Greedy Tricks
+ */
+
+/**
+ * 55. Jump Game
+ */
+func canJump(nums []int) bool {
+    reachablePosition := 0
+    for i := 0; i < len(nums); i++ {
+        if i > reachablePosition {
+            return false
+        }
+        if i + nums[i] > reachablePosition {
+            reachablePosition = i + nums[i]
+        }
+    }
+    return true
+}
+
+/**
+ * 45. Jump Game II
+ */
+func jump(nums []int) int {
+    curJump, farthestJump, jumps := 0, 0, 0
+    for i := 0; i < len(nums)-1; i++ {
+        // push index of furthest jump during current iteration
+        if i+nums[i] > farthestJump {
+            farthestJump = i + nums[i]
+        }
+
+        // if current iteration is ended - setup the next one
+        if i == curJump {
+            jumps, curJump = jumps+1, farthestJump
+
+            if curJump >= len(nums)-1 {
+                return jumps
+            }
+        }
+    }
+
+    return 0‰
+}
+
+/**
+ * Scheduling & Intervals
+ */
+
+/**
  * 134. Gas Station
  * Given two integer arrays gas and cost, return the starting gas station's index
  * if you can travel around the circuit once in the clockwise direction,
@@ -13,7 +61,7 @@ func canCompleteCircuit(gas []int, cost []int) int {
         globalFuelLeft += gas[i] - cost[i]
         fuelLeft += gas[i] - cost[i]
         if fuelLeft < 0 {
-            start = i + 1
+            start = i + 1 // always restart from the next station after failure
             fuelLeft = 0
         }
     }
@@ -23,6 +71,10 @@ func canCompleteCircuit(gas []int, cost []int) int {
     }
     return start
 }
+
+/**
+ * Tasks / Allocation
+ */
 
 /**
  * 621. Task Scheduler
