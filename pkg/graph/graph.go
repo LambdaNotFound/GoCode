7c31bec42d61be052f748992f28df348ca9b1191
LambdaNotFound <yiqyudev@gmail.com>
2025-08-08 18:21:30 -0700

    BFS, DFS

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,11 +1,56 @@
 package graph
 
-import . "gocode/types"
+import (
+	. "gocode/types"
+)
 
 /**
- * 133. Clone Graph
+ * BFS
+ */
+func BFS(start *Node) {
+    if start == nil {
+        return
+    }
+
+    visited := make(map[*Node]bool)
+    queue := []*Node{start}
+    visited[start] = true
+
+    for len(queue) > 0 {
+        curr := queue[0]
+        queue = queue[1:]
+
+        for _, neighbor := range curr.Neighbors {
+            if !visited[neighbor] {
+                visited[neighbor] = true
+                queue = append(queue, neighbor)
+            }
+        }
+    }
+}
+
+/**
+ * DFS
+ */
+func DFS(start *Node, visited map[*Node]bool) {
+    if start == nil {
+        return
+    }
+    if visited[start] {
+        return
+    }
+
+    visited[start] = true
+
+    for _, neighbor := range start.Neighbors {
+        DFS(neighbor, visited)
+    }
+}
+
+/**
+ * 133. Clone Graph - a connected undirected graph
  *
- * BFS + HashMap
+ * BFS + HashMap: queue, map[*Node]*Node
  */
 func cloneGraph(node *Node) *Node {
     if node == nil {
@@ -14,19 +59,21 @@ func cloneGraph(node *Node) *Node {
 
     queue := []*Node{node}
     mapToCloned := make(map[*Node]*Node)
-    mapToCloned[node] = &Node{Val: node.Val}
+    mapToCloned[node] = &Node{Val: node.Val} // tracks visited nodes
 
     for len(queue) > 0 {
-        curr := queue[0]
+        currNode := queue[0]
         queue = queue[1:]
 
-        for _, neighbor := range curr.Neighbors {
+        for _, neighbor := range currNode.Neighbors {
             if _, exist := mapToCloned[neighbor]; !exist {
                 mapToCloned[neighbor] = &Node{Val: neighbor.Val}
-                queue = append(queue, neighbor)
+                queue = append(queue, neighbor) // only enqueue un-visited
             }
 
-            mapToCloned[curr].Neighbors = append(mapToCloned[curr].Neighbors, mapToCloned[neighbor])
+            currNodeCopy := mapToCloned[currNode]
+            neighborCopy := mapToCloned[neighbor]
+            currNodeCopy.Neighbors = append(currNodeCopy.Neighbors, neighborCopy)
         }
     }
 
