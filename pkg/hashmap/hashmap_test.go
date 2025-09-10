657f8a49ae54ee80edc91fe577517b9d813018b9
LambdaNotFound <yiqyudev@gmail.com>
2025-09-09 23:45:23 -0700

    Test_twoSum

--- pkg/hashmap/hashmap_test.go
+++ pkg/hashmap/hashmap_test.go
@@ -6,6 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_twoSum(t *testing.T) {
+    tests := []struct {
+        name     string
+        nums     []int
+        target   int
+        expected []int
+    }{
+        {name: "basic_case", nums: []int{2, 7, 11, 15}, target: 9, expected: []int{0, 1}},
+        {name: "another_case", nums: []int{3, 2, 4}, target: 6, expected: []int{1, 2}},
+        {name: "repeated_numbers", nums: []int{3, 3}, target: 6, expected: []int{0, 1}},
+        {name: "larger_array", nums: []int{1, 2, 3, 4, 5, 6}, target: 11, expected: []int{4, 5}},
+        {name: "negatives", nums: []int{-1, -2, -3, -4, -5}, target: -8, expected: []int{2, 4}},
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            result := twoSum(tt.nums, tt.target)
+
+            assert.ElementsMatch(t, tt.expected, result)
+        })
+    }
+}
+
 func Test_isAnagram(t *testing.T) {
     tests := []struct {
         name     string
