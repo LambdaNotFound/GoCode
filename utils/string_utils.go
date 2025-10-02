98c641bbb94b339eca9be138df3c6a550eec7dcb
LambdaNotFound <yiqyudev@gmail.com>
2025-10-01 22:48:45 -0700

    UTF-8 string

--- utils/string_utils.go
+++ utils/string_utils.go
@@ -1,12 +1,23 @@
 package utils
 
+import "fmt"
+
 type String string
 
 func (str String) foo() string {
+    for i := 0; i < len(str); i++ {
+        b := str[i] // byte (uint8) everything’s ASCII, so 1 byte = 1 rune.
+        fmt.Printf("s[%d] = %c (byte value: %d)\n", i, b, b)
+    }
+
+    for i, r := range str {
+        fmt.Printf("index %d: rune %c (Unicode: %U)\n", i, r, r)
+    }
+
 	return string(str) + " method on custom type"
 }
 
 func substr(str string, start, end int) string {
-	runes := []rune(str)
+	runes := []rune(str) // convert to rune slice
 	return string(runes[start:end])
 }
