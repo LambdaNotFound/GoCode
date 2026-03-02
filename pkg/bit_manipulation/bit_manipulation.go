205c211629652444f79f996c485cda832992222c
LambdaNotFound <yiqyudev@gmail.com>
2026-03-02 12:51:29 -0800

    Counting Bits

--- pkg/bit_manipulation/bit_manipulation.go
+++ pkg/bit_manipulation/bit_manipulation.go
@@ -18,3 +18,42 @@ func getSum(a int, b int) int {
 	}
 	return sum
 }
+
+/*
+ * 191. Number of 1 Bits
+ * Time Complexity:
+ * If you treat the input as an arbitrary n and measure by its value,
+ * the number of bits is about log₂(n), so you get O(log n) iterations.
+ */
+func hammingWeight(n int) int {
+	count := 0
+	for n != 0 {
+		if n&1 == 1 {
+			count += 1
+		}
+		n = n >> 1
+	}
+
+	return count
+}
+
+/*
+ * 338. Counting Bits
+ *
+ * 0 --> 0    --> 0
+ * 1 --> 1    --> 1
+ * 2 --> 10   --> 1
+ * 3 --> 11   --> 2
+ * 4 --> 100  --> 1
+ * 5 --> 101  --> 2
+ * 6 --> 110  --> 2
+ * 7 --> 111  --> 3
+ * 8 --> 1000 --> 1
+ */
+func countBits(n int) []int {
+	ans := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		ans[i] = ans[i>>1] + (i & 1)
+	}
+	return ans
+}
