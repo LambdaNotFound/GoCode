d98ebb164ba9feb527e4ca61b2fd60b664a380b0
LambdaNotFound <yiqyudev@gmail.com>
2026-03-03 11:42:37 -0800

    cloneGraph

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -8,43 +8,43 @@ import (
  * BFS
  */
 func BFS(start *Node) {
-    if start == nil {
-        return
-    }
+	if start == nil {
+		return
+	}
 
-    visited := make(map[*Node]bool)
-    queue := []*Node{start}
-    visited[start] = true
+	visited := make(map[*Node]bool)
+	queue := []*Node{start}
+	visited[start] = true
 
-    for len(queue) > 0 {
-        curr := queue[0]
-        queue = queue[1:]
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
 
-        for _, neighbor := range curr.Neighbors {
-            if !visited[neighbor] {
-                visited[neighbor] = true
-                queue = append(queue, neighbor)
-            }
-        }
-    }
+		for _, neighbor := range curr.Neighbors {
+			if !visited[neighbor] {
+				visited[neighbor] = true
+				queue = append(queue, neighbor)
+			}
+		}
+	}
 }
 
 /**
  * DFS
  */
 func DFS(start *Node, visited map[*Node]bool) {
-    if start == nil {
-        return
-    }
-    if visited[start] {
-        return
-    }
+	if start == nil {
+		return
+	}
+	if visited[start] {
+		return
+	}
 
-    visited[start] = true
+	visited[start] = true
 
-    for _, neighbor := range start.Neighbors {
-        DFS(neighbor, visited)
-    }
+	for _, neighbor := range start.Neighbors {
+		DFS(neighbor, visited)
+	}
 }
 
 /**
@@ -53,59 +53,59 @@ func DFS(start *Node, visited map[*Node]bool) {
  * BFS + HashMap: queue, map[*Node]*Node
  */
 func cloneGraph(node *Node) *Node {
-    if node == nil {
-        return nil
-    }
+	if node == nil {
+		return nil
+	}
 
-    queue := []*Node{node}
-    mapToCloned := make(map[*Node]*Node)
-    mapToCloned[node] = &Node{Val: node.Val} // tracks visited nodes
+	queue := []*Node{node}
+	mapToCopy := make(map[*Node]*Node)
+	mapToCopy[node] = &Node{Val: node.Val} // tracks visited nodes
 
-    for len(queue) > 0 {
-        currNode := queue[0]
-        queue = queue[1:]
+	for len(queue) > 0 {
+		currNode := queue[0]
+		queue = queue[1:]
+		currNodeCopy := mapToCopy[currNode]
 
-        for _, neighbor := range currNode.Neighbors {
-            if _, exist := mapToCloned[neighbor]; !exist {
-                mapToCloned[neighbor] = &Node{Val: neighbor.Val}
-                queue = append(queue, neighbor) // only enqueue un-visited
-            }
+		for _, neighbor := range currNode.Neighbors {
+			if _, exist := mapToCopy[neighbor]; !exist {
+				mapToCopy[neighbor] = &Node{Val: neighbor.Val}
+				queue = append(queue, neighbor) // only enqueue un-visited
+			}
 
-            currNodeCopy := mapToCloned[currNode]
-            neighborCopy := mapToCloned[neighbor]
-            currNodeCopy.Neighbors = append(currNodeCopy.Neighbors, neighborCopy)
-        }
-    }
+			neighborCopy := mapToCopy[neighbor]
+			currNodeCopy.Neighbors = append(currNodeCopy.Neighbors, neighborCopy)
+		}
+	}
 
-    return mapToCloned[node]
+	return mapToCopy[node]
 }
 
 func cloneGraph_DFS(node *Node) *Node {
-    visited := make(map[*Node]*Node) // map[ptr]ptr
+	visited := make(map[*Node]*Node) // map[ptr]ptr
 
-    var dfs func(*Node) *Node
-    dfs = func(node *Node) *Node {
-        if node == nil {
-            return nil
-        }
+	var dfs func(*Node) *Node
+	dfs = func(node *Node) *Node {
+		if node == nil {
+			return nil
+		}
 
-        // If the node is already cloned, return it
-        if clone, exist := visited[node]; exist {
-            return clone
-        }
+		// If the node is already cloned, return it
+		if copy, exist := visited[node]; exist {
+			return copy
+		}
 
-        // Create a new clone
-        clone := &Node{Val: node.Val}
-        visited[node] = clone // mark as cloned before visiting neighbors
+		// Create a new clone
+		copy := &Node{Val: node.Val}
+		visited[node] = copy // mark as cloned before visiting neighbors
 
-        // Recurse on neighbors
-        for _, neighbor := range node.Neighbors {
-            neighborClone := dfs(neighbor)
-            clone.Neighbors = append(clone.Neighbors, neighborClone)
-            // clone.Neighbors = append(clone.Neighbors, dfs(neighbor))
-        }
-        return clone
-    }
+		// Recurse on neighbors
+		for _, neighbor := range node.Neighbors {
+			neighborClone := dfs(neighbor)
+			copy.Neighbors = append(copy.Neighbors, neighborClone)
+			// clone.Neighbors = append(clone.Neighbors, dfs(neighbor))
+		}
+		return copy
+	}
 
-    return dfs(node)
+	return dfs(node)
 }
