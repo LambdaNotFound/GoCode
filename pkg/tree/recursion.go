699b96845e9429be8ef0fae7ab0252663cf81e2e
LambdaNotFound <yiqyudev@gmail.com>
2026-03-04 00:56:24 -0800

    100. Same Tree

---
+++ pkg/tree/recursion.go
@@ -0,0 +1,24 @@
+package tree
+
+import (
+	. "gocode/types"
+)
+
+/*
+ * 100. Same Tree
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if (p != nil && q == nil) || (p == nil && q != nil) {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
