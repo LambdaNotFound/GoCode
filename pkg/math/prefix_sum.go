c16a089428949f27c444e10cc14a4094c211a571
LambdaNotFound <yiqyudev@gmail.com>
2026-03-03 23:54:38 -0800

    productExceptSelfWithMultiplier

--- pkg/math/prefix_sum.go
+++ pkg/math/prefix_sum.go
@@ -10,38 +10,38 @@ package math
  * ANS  [bcd   acd   abd   abc]
  *
  */
-func productExceptSelf(nums []int) []int {
-    l := len(nums)
-    left := make([]int, l)
-    right := make([]int, l)
-    ans := make([]int, l)
-    left[0] = 1
-    right[l-1] = 1
-    for i := 1; i < l; i++ {
-        j := l - i - 1
-        left[i] = nums[i-1] * left[i-1]
-        right[j] = nums[j+1] * right[j+1]
-    }
-    for i := 0; i < l; i++ {
-        ans[i] = left[i] * right[i]
-    }
-    return ans
+func productExceptSelfWithMultiplier(nums []int) []int {
+	res := make([]int, len(nums))
+
+	multiplier := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = multiplier
+		multiplier *= nums[i]
+	}
+
+	multiplier = 1
+	for i := len(res) - 1; i >= 0; i-- {
+		res[i] *= multiplier
+		multiplier *= nums[i]
+	}
+
+	return res
 }
 
-func productExceptSelfWithMultiplier(nums []int) []int {
-    res := make([]int, len(nums))
-
-    multiplier := 1
-    for i := 0; i < len(nums); i++ {
-        res[i] = multiplier
-        multiplier *= nums[i]
-    }
-
-    multiplier = 1
-    for i := len(res) - 1; i >= 0; i-- {
-        res[i] *= multiplier
-        multiplier *= nums[i]
-    }
-
-    return res
+func productExceptSelf(nums []int) []int {
+	l := len(nums)
+	left := make([]int, l)
+	right := make([]int, l)
+	ans := make([]int, l)
+	left[0] = 1
+	right[l-1] = 1
+	for i := 1; i < l; i++ {
+		j := l - i - 1
+		left[i] = nums[i-1] * left[i-1]
+		right[j] = nums[j+1] * right[j+1]
+	}
+	for i := 0; i < l; i++ {
+		ans[i] = left[i] * right[i]
+	}
+	return ans
 }
