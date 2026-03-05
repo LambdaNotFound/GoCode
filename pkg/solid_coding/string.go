1d865214d281e029c2d284c862fd6ff474486160
LambdaNotFound <yiqyudev@gmail.com>
2026-03-04 16:03:19 -0800

    49. Group Anagrams

--- pkg/solid_coding/string.go
+++ pkg/solid_coding/string.go
@@ -2,6 +2,7 @@ package solid_coding
 
 import (
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 	"unicode"
@@ -19,128 +20,128 @@ import (
  * 8. String to Integer (atoi)
  */
 func myAtoi(s string) int {
-    s = strings.TrimSpace(s)
-    multiplier := 1
-    if len(s) == 0 {
-        return 0
-    } else if s[0] == '-' {
-        multiplier = -1
-        s = s[1:]
-    } else if s[0] == '+' {
-        s = s[1:]
-    }
+	s = strings.TrimSpace(s)
+	multiplier := 1
+	if len(s) == 0 {
+		return 0
+	} else if s[0] == '-' {
+		multiplier = -1
+		s = s[1:]
+	} else if s[0] == '+' {
+		s = s[1:]
+	}
 
-    res := 0
-    for _, r := range s {
-        if !unicode.IsDigit(r) {
-            break
-        }
-        curr, _ := strconv.Atoi(string(r))
+	res := 0
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			break
+		}
+		curr, _ := strconv.Atoi(string(r))
 
-        if multiplier == 1 && (res*10 > math.MaxInt32-curr) {
-            return math.MaxInt32
-        }
-        if multiplier == -1 && (-res*10 < math.MinInt32+curr) {
-            return math.MinInt32
-        }
+		if multiplier == 1 && (res*10 > math.MaxInt32-curr) {
+			return math.MaxInt32
+		}
+		if multiplier == -1 && (-res*10 < math.MinInt32+curr) {
+			return math.MinInt32
+		}
 
-        res = res*10 + curr
-    }
-    return multiplier * res
+		res = res*10 + curr
+	}
+	return multiplier * res
 }
 
 /**
  * 20. Valid Parentheses
  */
 func isValid(s string) bool {
-    stack := []byte{}
-    for i := 0; i < len(s); i++ {
-        if s[i] == '(' || s[i] == '[' || s[i] == '{' {
-            stack = append(stack, s[i])
-        } else {
-            l := len(stack)
-            if l == 0 {
-                return false
-            } else {
-                var expected byte
-                switch s[i] {
-                case ')':
-                    expected = '('
-                case ']':
-                    expected = '['
-                case '}':
-                    expected = '{'
-                }
-                if expected != stack[l-1] {
-                    return false
-                }
-            }
-            stack = stack[:l-1]
-        }
+	stack := []byte{}
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		} else {
+			l := len(stack)
+			if l == 0 {
+				return false
+			} else {
+				var expected byte
+				switch s[i] {
+				case ')':
+					expected = '('
+				case ']':
+					expected = '['
+				case '}':
+					expected = '{'
+				}
+				if expected != stack[l-1] {
+					return false
+				}
+			}
+			stack = stack[:l-1]
+		}
 
-    }
+	}
 
-    return len(stack) == 0
+	return len(stack) == 0
 }
 
 func isValid_lookup(s string) bool {
-    stack := []rune{} // Stack for opening brackets
-    hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
+	stack := []rune{} // Stack for opening brackets
+	hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
 
-    for _, char := range s {
-        if match, found := hash[char]; found {
-            // Check if stack is non-empty and matches
-            if len(stack) > 0 && stack[len(stack)-1] == match {
-                stack = stack[:len(stack)-1] // Pop
-            } else {
-                return false // Invalid
-            }
-        } else {
-            stack = append(stack, char) // Push opening bracket
-        }
-    }
-    return len(stack) == 0 // Valid if stack is empty
+	for _, char := range s {
+		if match, found := hash[char]; found {
+			// Check if stack is non-empty and matches
+			if len(stack) > 0 && stack[len(stack)-1] == match {
+				stack = stack[:len(stack)-1] // Pop
+			} else {
+				return false // Invalid
+			}
+		} else {
+			stack = append(stack, char) // Push opening bracket
+		}
+	}
+	return len(stack) == 0 // Valid if stack is empty
 }
 
 /**
  * 67. Add Binary
  */
 func addBinary(a string, b string) string {
-    finalstr := ""
-    v1, v2, rem := 0, 0, 0
+	finalstr := ""
+	v1, v2, rem := 0, 0, 0
 
-    for l1, l2 := len(a)-1, len(b)-1; l1 >= 0 || l2 >= 0 || rem != 0; {
-        if l1 >= 0 {
-            v1, _ = strconv.Atoi(string(a[l1]))
-        }
-        if l2 >= 0 {
-            v2, _ = strconv.Atoi(string(b[l2]))
-        }
+	for l1, l2 := len(a)-1, len(b)-1; l1 >= 0 || l2 >= 0 || rem != 0; {
+		if l1 >= 0 {
+			v1, _ = strconv.Atoi(string(a[l1]))
+		}
+		if l2 >= 0 {
+			v2, _ = strconv.Atoi(string(b[l2]))
+		}
 
-        sum := v1 + v2 + rem
+		sum := v1 + v2 + rem
 
-        // according to sum append appropriate character in finalstr
-        switch sum {
-        case 3:
-            finalstr = "1" + finalstr
-            rem = 1
-        case 2:
-            finalstr = "0" + finalstr
-            rem = 1
-        case 1:
-            finalstr = "1" + finalstr
-            rem = 0
-        case 0:
-            finalstr = "0" + finalstr
-            rem = 0
-        }
+		// according to sum append appropriate character in finalstr
+		switch sum {
+		case 3:
+			finalstr = "1" + finalstr
+			rem = 1
+		case 2:
+			finalstr = "0" + finalstr
+			rem = 1
+		case 1:
+			finalstr = "1" + finalstr
+			rem = 0
+		case 0:
+			finalstr = "0" + finalstr
+			rem = 0
+		}
 
-        v1, v2 = 0, 0
-        l1 -= 1
-        l2 -= 1
-    }
+		v1, v2 = 0, 0
+		l1 -= 1
+		l2 -= 1
+	}
 
-    return finalstr
+	return finalstr
 }
 
 /**
@@ -149,19 +150,45 @@ func addBinary(a string, b string) string {
  * return the length of the longest palindrome that can be built with those letters
  */
 func longestPalindromeLength(s string) int {
-    res := 0
-    charMap := make(map[rune]int)
-    for _, val := range s {
-        charMap[val] += 1
-        count := charMap[val]
-        if count == 2 {
-            res += 2
-            delete(charMap, val)
-        }
-    }
+	res := 0
+	charMap := make(map[rune]int)
+	for _, val := range s {
+		charMap[val] += 1
+		count := charMap[val]
+		if count == 2 {
+			res += 2
+			delete(charMap, val)
+		}
+	}
 
-    if len(charMap) != 0 {
-        res += 1
-    }
-    return res
+	if len(charMap) != 0 {
+		res += 1
+	}
+	return res
+}
+
+/*
+ * 49. Group Anagrams
+ */
+func groupAnagrams(strs []string) [][]string {
+	var sortRunes func(string) string
+	sortRunes = func(s string) string {
+		runes := []rune(s)
+		sort.Slice(runes, func(i, j int) bool {
+			return runes[i] < runes[j]
+		})
+		return string(runes)
+	}
+
+	anagramsMap := make(map[string][]string)
+	for _, str := range strs {
+		key := sortRunes(str)
+		anagramsMap[key] = append(anagramsMap[key], str)
+	}
+
+	res := make([][]string, 0)
+	for _, anagrams := range anagramsMap {
+		res = append(res, anagrams)
+	}
+	return res
 }
