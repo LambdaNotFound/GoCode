7ced1af5fdaa65fb66c2dbf146e1935f01a9ac83
LambdaNotFound <yiqyudev@gmail.com>
2026-03-09 13:48:07 -0700

    62. Unique Paths

--- pkg/api_design/lru_cache.go
+++ pkg/api_design/lru_cache.go
@@ -6,113 +6,116 @@ import (
 
 /**
  * 146. LRU Cache
+ *
+ * 1. hashmap + doubly linked list
+ * 2. container/list
  */
 type DoublyLinkedList struct {
-    key  int
-    val  int
-    prev *DoublyLinkedList
-    next *DoublyLinkedList
+	key  int
+	val  int
+	prev *DoublyLinkedList
+	next *DoublyLinkedList
 }
 
 type LRUCache struct {
-    capacity int
-    head     *DoublyLinkedList
-    tail     *DoublyLinkedList
-    cache    map[int]*DoublyLinkedList
+	capacity int
+	head     *DoublyLinkedList
+	tail     *DoublyLinkedList
+	cache    map[int]*DoublyLinkedList // map cache value > double linked list node
 }
 
 func ConstructorLRUCache(capacity int) LRUCache {
-    head := &DoublyLinkedList{key: 0, val: 0}
-    tail := &DoublyLinkedList{key: 0, val: 0}
-    head.next = tail
-    tail.prev = head
-    return LRUCache{
-        capacity: capacity,
-        head:     head,
-        tail:     tail,
-        cache:    make(map[int]*DoublyLinkedList),
-    }
+	head := &DoublyLinkedList{key: 0, val: 0}
+	tail := &DoublyLinkedList{key: 0, val: 0}
+	head.next = tail
+	tail.prev = head
+	return LRUCache{
+		capacity: capacity,
+		head:     head,
+		tail:     tail,
+		cache:    make(map[int]*DoublyLinkedList),
+	}
 }
 
 func (l *LRUCache) remove(node *DoublyLinkedList) {
-    prev := node.prev
-    next := node.next
-    prev.next = next
-    next.prev = prev
+	prev := node.prev
+	next := node.next
+	prev.next = next
+	next.prev = prev
 }
 
 func (l *LRUCache) insert(node *DoublyLinkedList) {
-    headNext := l.head.next
-    l.head.next = node
-    node.prev = l.head
-    node.next = headNext
-    headNext.prev = node
+	headNext := l.head.next
+	l.head.next = node
+	node.prev = l.head
+	node.next = headNext
+	headNext.prev = node
 }
 
 func (l *LRUCache) Get(key int) int {
-    if node, ok := l.cache[key]; ok {
-        l.remove(node)
-        l.insert(node)
-        return node.val
-    }
-    return -1
+	if node, ok := l.cache[key]; ok {
+		l.remove(node)
+		l.insert(node)
+		return node.val
+	}
+	return -1
 }
 
 func (l *LRUCache) Put(key int, value int) {
-    if node, ok := l.cache[key]; ok {
-        l.remove(node)
-        node.val = value
-        l.insert(node)
-    } else {
-        newNode := &DoublyLinkedList{key: key, val: value}
-        l.cache[key] = newNode
-        l.insert(newNode)
-        if len(l.cache) > l.capacity {
-            lru := l.tail.prev
-            l.remove(lru)
-            delete(l.cache, lru.key)
-        }
-    }
+	if node, ok := l.cache[key]; ok {
+		l.remove(node)
+		node.val = value
+		l.insert(node)
+	} else {
+		newNode := &DoublyLinkedList{key: key, val: value}
+		l.cache[key] = newNode
+		l.insert(newNode)
+		if len(l.cache) > l.capacity {
+			lru := l.tail.prev
+			l.remove(lru)
+			delete(l.cache, lru.key)
+		}
+	}
 }
 
 /**
  * w/ "container/list"
  */
 type LRUCacheWithList[K comparable] struct {
-    capacity int
-    list     *list.List
-    hashmap  map[K]*list.Element
+	capacity int
+	list     *list.List
+	hashmap  map[K]*list.Element
 }
 
 func ConstructorLRUCacheWithList[K comparable](capacity int) LRUCacheWithList[K] {
-    return LRUCacheWithList[K]{
-        capacity: capacity,
-        list:     list.New(),
-        hashmap:  make(map[K]*list.Element),
-    }
+	return LRUCacheWithList[K]{
+		capacity: capacity,
+		list:     list.New(),
+		hashmap:  make(map[K]*list.Element),
+	}
 }
 
 func (l *LRUCacheWithList[K]) Get(key K) any {
-    if node, ok := l.hashmap[key]; ok {
-        value := l.list.Remove(node)
-        e := l.list.PushFront(value)
-        l.hashmap[key] = e
-        return node.Value
-    }
-    return nil
+	if node, ok := l.hashmap[key]; ok {
+		value := l.list.Remove(node)
+		e := l.list.PushFront(value)
+		l.hashmap[key] = e
+		return node.Value
+	}
+	return nil
 }
 
 func (l *LRUCacheWithList[K]) Put(key K, value any) {
-    if node, ok := l.hashmap[key]; ok {
-        l.list.Remove(node)
-        l.list.PushFront(value)
-    } else {
-        e := l.list.PushFront(value)
-        l.hashmap[key] = e
-        if l.list.Len() > l.capacity {
-            back := l.list.Back()
-            l.list.Remove(back)
-            delete(l.hashmap, back.Value.(K))
-        }
-    }
+	if node, ok := l.hashmap[key]; ok {
+		l.list.Remove(node)
+		l.list.PushFront(value)
+	} else {
+		e := l.list.PushFront(value)
+		l.hashmap[key] = e
+		if l.list.Len() > l.capacity {
+			back := l.list.Back()
+			l.list.Remove(back)
+			delete(l.hashmap, back.Value.(K))
+		}
+	}
 }
--- pkg/dynamic_programming/unique_paths.go
+++ pkg/dynamic_programming/unique_paths.go
@@ -11,37 +11,35 @@ package dynamic_programming
  *     dp[i, j] = dp[i-1][j] + dp[i][j-1]
  */
 func uniquePaths(m int, n int) int {
-    dp := make([]int, n)
-    for j := 0; j < n; j++ {
-        dp[j] = 1
-    }
+	dp := make([]int, n)
+	for j := 0; j < n; j++ { // optimized for space
+		dp[j] = 1
+	}
 
-    for i := 1; i < m; i++ {
-        for j := 1; j < n; j++ {
-            dp[j] += dp[j-1]
-        }
-    }
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
+		}
+	}
 
-    return dp[n-1]
+	return dp[n-1]
 }
 
 func uniquePathsNaive(m int, n int) int {
-    dp := make([][]int, m)
-    for i := range dp {
-        dp[i] = make([]int, n)
-    }
-    for i := 0; i < m; i++ {
-        dp[i][0] = 1
-    }
-    for j := 0; j < n; j++ {
-        dp[0][j] = 1
-    }
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+	}
 
-    for i := 1; i < m; i++ {
-        for j := 1; j < n; j++ {
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        }
-    }
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				table[i][j] = 1
+			} else {
+				table[i][j] = table[i-1][j] + table[i][j-1]
+			}
+		}
+	}
 
-    return dp[m-1][n-1]
+	return table[m-1][n-1]
 }
