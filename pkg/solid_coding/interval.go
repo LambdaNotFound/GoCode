8bbf72fd64ff76315260af2b20dc9fdd077be2ee
LambdaNotFound <yiqyudev@gmail.com>
2026-03-10 14:33:22 -0700

    Interval

--- pkg/solid_coding/interval.go
+++ pkg/solid_coding/interval.go
@@ -6,6 +6,72 @@ import (
 )
 
 /**
+ * 57. Insert Interval
+ */
+func insert(intervals [][]int, newInterval []int) [][]int {
+	res := make([][]int, 0)
+
+	i := 0
+	for ; i < len(intervals) && intervals[i][1] < newInterval[0]; i++ {
+		res = append(res, intervals[i])
+	}
+	for ; i < len(intervals) && intervals[i][0] <= newInterval[1]; i++ {
+		newInterval[0] = min(intervals[i][0], newInterval[0])
+		newInterval[1] = max(intervals[i][1], newInterval[1])
+	}
+
+	res = append(res, newInterval)
+	for i < len(intervals) {
+		res = append(res, intervals[i])
+		i++
+	}
+	return res
+}
+
+func insertWithSlice(intervals [][]int, newInterval []int) [][]int {
+	before, after := make([][]int, 0), make([][]int, 0)
+	for i := 0; i < len(intervals); i++ {
+		cur := intervals[i]
+		if cur[1] < newInterval[0] {
+			before = append(before, cur)
+		} else if newInterval[1] < cur[0] {
+			after = append(after, cur)
+		} else {
+			newInterval[0] = min(newInterval[0], cur[0])
+			newInterval[1] = max(newInterval[1], cur[1])
+		}
+	}
+	res := append(before, newInterval)
+	res = append(res, after...)
+	return res
+}
+
+/**
+ * 56. Merge Intervals
+ *
+ * [[1,4],[2,3]] => [[1,4]]
+ */
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	result := [][]int{intervals[0]}
+	for _, interval := range intervals {
+		current := result[len(result)-1]
+		if current[1] < interval[0] {
+			result = append(result, interval)
+		} else {
+			if current[1] < interval[1] {
+				current[1] = interval[1]
+			}
+		}
+	}
+
+	return result
+}
+
+/**
  * Meeting Rooms
  *
  */
@@ -26,6 +92,7 @@ func canAttendMeetings(intervals []Interval) bool {
 /**
  * Meeting Rooms II
  *
+ * whats next earliest start time, next earliest end time?
  */
 func minMeetingRooms(intervals []Interval) int {
 	start, end := make([]int, len(intervals)), make([]int, len(intervals))
--- pkg/solid_coding/solid_coding.go
+++ pkg/solid_coding/solid_coding.go
@@ -2,7 +2,6 @@ package solid_coding
 
 import (
 	. "gocode/utils"
-	"sort"
 )
 
 /**
@@ -47,55 +46,5 @@ func spiralOrder(matrix [][]int) []int {
 		direction = nextDirection[direction]
 	}
 
-	return res
-}
-
-/**
- * 56. Merge Intervals
- *
- * [[1,4],[2,3]] => [[1,4]]
- */
-func merge(intervals [][]int) [][]int {
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
-	})
-
-	result := [][]int{}
-	result = append(result, intervals[0])
-	for _, interval := range intervals {
-		current := result[len(result)-1]
-		if current[1] < interval[0] {
-			result = append(result, interval)
-		} else {
-			if current[1] < interval[1] {
-				current[1] = interval[1]
-			}
-		}
-	}
-
-	return result
-}
-
-/**
- * 57. Insert Interval
- */
-func insert(intervals [][]int, newInterval []int) [][]int {
-	res := make([][]int, 0)
-
-	i := 0
-	for ; i < len(intervals) && intervals[i][1] < newInterval[0]; i++ {
-		res = append(res, intervals[i])
-	}
-	for ; i < len(intervals) && intervals[i][0] <= newInterval[1]; i++ {
-		// only update the newInterval[min, max]
-		newInterval[0] = Min(intervals[i][0], newInterval[0])
-		newInterval[1] = Max(intervals[i][1], newInterval[1])
-	}
-
-	res = append(res, newInterval)
-	for i < len(intervals) {
-		res = append(res, intervals[i])
-		i++
-	}
 	return res
 }
