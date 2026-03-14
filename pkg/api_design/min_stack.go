364f96f844285b48328b2d46c2f128252959ffa7
LambdaNotFound <yiqyudev@gmail.com>
2026-03-13 17:54:37 -0700

    155. Min Stack
    use 2 stacks, 1 for tracking min value.
    
    check min stack's length before GET
    
    only pop min stack if data stack value == top of min stack

--- pkg/api_design/min_stack.go
+++ pkg/api_design/min_stack.go
@@ -4,51 +4,41 @@ package apidesign
  * 155. Min Stack
  *
  * MinStack() initializes the stack object.
- * void push(int val) pushes the element val onto the stack.
- * void pop() removes the element on the top of the stack.
- * int top() gets the top element of the stack.
- * int getMin() retrieves the minimum element in the stack.
+ *     void push(int val) pushes the element val onto the stack.
+ *     void pop() removes the element on the top of the stack.
+ *     int top() gets the top element of the stack.
+ *     int getMin() retrieves the minimum element in the stack.
+ *
  * You must implement a solution with O(1) time complexity for each function.
  */
 
 type MinStack struct {
-    stack    []int
-    minStack []int
+	data []int // elements stack
+	mins []int // minimums stack
 }
 
 func ConstructorMinStack() MinStack {
-    return MinStack{
-        stack:    []int{},
-        minStack: []int{},
-    }
+	return MinStack{}
 }
 
-func (m *MinStack) Push(val int) {
-    m.stack = append(m.stack, val)
-
-    len := len(m.minStack)
-    if len == 0 || m.minStack[len-1] > val {
-        m.minStack = append(m.minStack, val)
-    } else {
-        m.minStack = append(m.minStack, m.minStack[len-1])
-    }
+func (s *MinStack) Push(val int) {
+	s.data = append(s.data, val)
+	if len(s.mins) == 0 || val <= s.GetMin() {
+		s.mins = append(s.mins, val)
+	}
 }
 
-func (m *MinStack) Pop() {
-    len := len(m.stack)
-
-    m.stack = m.stack[:len-1]
-    m.minStack = m.minStack[:len-1]
+func (s *MinStack) Pop() {
+	if s.Top() == s.GetMin() {
+		s.mins = s.mins[:len(s.mins)-1]
+	}
+	s.data = s.data[:len(s.data)-1]
 }
 
-func (m *MinStack) Top() int {
-    len := len(m.stack)
-
-    return m.stack[len-1]
+func (s *MinStack) Top() int {
+	return s.data[len(s.data)-1]
 }
 
-func (m *MinStack) GetMin() int {
-    len := len(m.minStack)
-
-    return m.minStack[len-1]
+func (s *MinStack) GetMin() int {
+	return s.mins[len(s.mins)-1]
 }
