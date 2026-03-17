22481499648957a675ea7df88873ad5b1885f37f
LambdaNotFound <yiqyudev@gmail.com>
2026-03-17 00:54:24 -0700

    Narayana's algoritm

--- pkg/backtracking/backtracking.go
+++ pkg/backtracking/backtracking.go
@@ -117,8 +117,8 @@ func permuteWithSliceSpread(nums []int) [][]int {
  *   → skipping me prevents duplicate permutations
  *
  *   visited[i-1] means: "my identical left sibling IS currently in the path"
- *	→ sibling is at a DIFFERENT position than me
- *	→ picking me here creates a genuinely different permutation ✅
+ *	 → sibling is at a DIFFERENT position than me
+ *	 → picking me here creates a genuinely different permutation ✅
  */
 func permuteUnique(nums []int) [][]int {
 	sort.Ints(nums)
@@ -151,6 +151,32 @@ func permuteUnique(nums []int) [][]int {
 
 	dfs([]int{})
 	return res
+}
+
+/*
+ * 31. Next Permutation
+ *
+ * Narayana's algoritm
+ */
+// 1. Search max j: nums[j] > nums[j-1]
+// 2. Search max l: l >= j && nums[l] > nums[j-1]
+// 3. Swap nums[j-1], nums[l]
+// 4. Reverse nums[j:]
+// For example:
+// 123654 -> 124653 -> 124356
+func nextPermutation(nums []int) {
+	j := len(nums) - 1
+	for ; j > 0 && nums[j-1] >= nums[j]; j-- {
+	}
+	if j != 0 {
+		l := len(nums) - 1
+		for ; l > j-1 && nums[j-1] >= nums[l]; l-- {
+		}
+		nums[j-1], nums[l] = nums[l], nums[j-1]
+	}
+	for k := len(nums) - 1; j < k; j, k = j+1, k-1 {
+		nums[j], nums[k] = nums[k], nums[j]
+	}
 }
 
 /**
