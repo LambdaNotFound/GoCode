3b0c3308d713266290291a4bcb5f7b26138ed30c
LambdaNotFound <yiqyudev@gmail.com>
2026-03-18 14:11:50 -0700

    1143. Longest Common Subsequence
    // LCS — answer is always last cell
    return dp[m][n]  ← always correct
    
    // LIS — answer is MAX across all cells
    // because LIS ending at each position varies
    res := 0
    for i := range dp {
        res = max(res, dp[i])  ← must scan all cells
    }

--- pkg/dynamic_programming/LCS.go
+++ pkg/dynamic_programming/LCS.go
@@ -2,30 +2,25 @@ package dynamic_programming
 
 /**
  * 1143. Longest Common Subsequence
+ *
+ * dp[i][j] = length of LCS of text1[0..i-1] and text2[0..j-1]
  */
-func longestCommonSubsequence(text1 string, text2 string) int {
-	if len(text1) > len(text2) {
-		return longestCommonSubsequence(text2, text1)
-	}
-
+func longestCommonSubsequence(text1, text2 string) int {
 	m, n := len(text1), len(text2)
 	dp := make([][]int, m+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
 	}
 
-	res := 0
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
 			if text1[i-1] == text2[j-1] {
-				dp[i][j] = 1 + dp[i-1][j-1]
+				dp[i][j] = dp[i-1][j-1] + 1
 			} else {
 				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 			}
-
-			res = max(res, dp[i][j])
 		}
 	}
 
-	return res
+	return dp[m][n]
 }
