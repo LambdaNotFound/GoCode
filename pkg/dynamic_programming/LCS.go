0ada5c239538a6d78eca61ac7a5f5ebab8baaa9c
LambdaNotFound <yiqyudev@gmail.com>
2026-03-18 14:40:49 -0700

    Longest Common Continuous Subarray

--- pkg/dynamic_programming/LCS.go
+++ pkg/dynamic_programming/LCS.go
@@ -24,3 +24,33 @@ func longestCommonSubsequence(text1, text2 string) int {
 
 	return dp[m][n]
 }
+
+/**
+ * 718. Maximum Length of Repeated Subarray
+ * Longest Common Continuous Subarray
+ *
+ * Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears
+ * in both arrays.
+ *
+ * dp[i][j] where dp[i][j] represents the length of the longest common suffix of A[0...i-1] and B[0...j-1]
+ *                                                                      ending at i, j
+ */
+func findLength(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	res := 0
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			}
+			res = max(res, dp[i][j])
+		}
+	}
+
+	return res
+}
