5a4c0be1549bcb7eb881974ea715c31d013569f6
LambdaNotFound <yiqyudev@gmail.com>
2026-03-18 21:45:00 -0700

    Rotate Image/Matrix
    matrix tricks

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -1,11 +1,33 @@
 package math
 
+import "slices"
+
 /**
  * 189. Rotate Array
  */
-func rotate(nums []int, k int) {
+func rotateArraySlicesReverse(nums []int, k int) {
+	index := len(nums) - k%len(nums)
+	slices.Reverse(nums[:index])
+	slices.Reverse(nums[index:])
+	slices.Reverse(nums)
+}
+
+func rotateArray(nums []int, k int) {
 	l := len(nums)
 	if len(nums) != 0 {
 		copy(nums, append(nums[l-k%l:], nums[0:l-k%l]...))
+	}
+}
+
+/**
+ * 48. Rotate Image
+ */
+func rotateImage(matrix [][]int) {
+	slices.Reverse(matrix)
+
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix[0]); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
 	}
 }
