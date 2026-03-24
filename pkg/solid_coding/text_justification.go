e78085740150392bafc1e2d5878b64ee1df853b7
LambdaNotFound <yiqyudev@gmail.com>
2026-03-24 00:54:14 -0700

    68. Text Justification

--- pkg/solid_coding/text_justification.go
+++ pkg/solid_coding/text_justification.go
@@ -11,33 +11,6 @@ import "strings"
  * How do you differentiate the last line from regular lines?
  *
  */
-func fullJustify(words []string, maxWidth int) []string {
-	var res []string
-	var cur []string
-	num_of_letters := 0
-
-	for _, word := range words {
-		if len(word)+len(cur)+num_of_letters > maxWidth {
-			for i := 0; i < maxWidth-num_of_letters; i++ {
-				cur[i%max(1, len(cur)-1)] += " "
-			}
-			res = append(res, strings.Join(cur, ""))
-			cur = cur[:0]
-			num_of_letters = 0
-		}
-		cur = append(cur, word)
-		num_of_letters += len(word)
-	}
-
-	lastLine := strings.Join(cur, " ")
-	for len(lastLine) < maxWidth {
-		lastLine += " "
-	}
-	res = append(res, lastLine)
-
-	return res
-}
-
 func fullJustifyCalude(words []string, maxWidth int) []string {
 	res := make([]string, 0)
 	line := make([]string, 0)
@@ -76,8 +49,7 @@ func justify(line []string, lineLetters, maxWidth int) string {
 	var sb strings.Builder
 	for i, word := range line {
 		sb.WriteString(word)
-		if i < gaps {
-			// distribute extra spaces to leftmost gaps first
+		if i < gaps { // distribute extra spaces to leftmost gaps first
 			spaces := spacePerGap
 			if i < extraSpaces {
 				spaces++
