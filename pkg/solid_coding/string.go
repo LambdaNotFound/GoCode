12d33526b528a1d9f56088a76f9aad5a3f78244c
LambdaNotFound <yiqyudev@gmail.com>
2026-03-23 17:09:17 -0700

    179. Largest Number
    return strs[i]+strs[j] > strs[j]+strs[i]

--- pkg/solid_coding/string.go
+++ pkg/solid_coding/string.go
@@ -236,3 +236,26 @@ func longestCommonPrefix(strs []string) string {
 	}
 	return res
 }
+
+/**
+ * 179. Largest Number
+ *
+ * sort strings
+ */
+func largestNumber(nums []int) string {
+	strs := make([]string, len(nums))
+	for i, num := range nums {
+		strs[i] = strconv.Itoa(num)
+	}
+
+	sort.Slice(strs, func(i, j int) bool {
+		return strs[i]+strs[j] > strs[j]+strs[i]
+	})
+
+	// edge case: all zeros
+	if strs[0] == "0" {
+		return "0"
+	}
+
+	return strings.Join(strs, "")
+}
