1ae0a39f86aeba4697eb2507ec4d685eccaf528f
LambdaNotFound <yiqyudev@gmail.com>
2026-03-24 17:27:55 -0700

    994. Rotting Oranges
    		if len(queue) > 0 {
    			minute++
    		}

--- pkg/graph/BFS.go
+++ pkg/graph/BFS.go
@@ -60,46 +60,47 @@ func orangesRotting(grid [][]int) int {
 
 func orangesRotting_slice(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
-	queue := [][2]int{} // queue of [row, col]
-	for r := 0; r < m; r++ {
-		for c := 0; c < n; c++ {
-			if grid[r][c] == 2 {
-				queue = append(queue, [2]int{r, c})
+	queue := [][]int{}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 2 {
+				queue = append(queue, []int{i, j})
+			}
+			if grid[i][j] == 1 {
 			}
 		}
 	}
 
-	directions := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-
-	minutes := 0
+	dirs := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	minute := 0
 	for len(queue) > 0 {
-		for _, orange := range queue {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			front := queue[0]
 			queue = queue[1:]
-			for _, dir := range directions {
-				r, c := orange[0]+dir[0], orange[1]+dir[1]
-				if r < 0 || r >= m || c < 0 || c >= n {
-					continue
+
+			for _, d := range dirs {
+				r, c := front[0]+d[0], front[1]+d[1]
+				if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1 {
+					grid[r][c] = 2
+					queue = append(queue, []int{r, c})
+
 				}
-				if grid[r][c] != 1 {
-					continue
-				}
-				grid[r][c] = 2
-				queue = append(queue, [2]int{r, c})
 			}
 		}
 		if len(queue) > 0 {
-			minutes++
+			minute++
 		}
 	}
 
-	for r := 0; r < m; r++ {
-		for c := 0; c < n; c++ {
-			if grid[r][c] == 1 {
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
 				return -1
 			}
 		}
 	}
-	return minutes
+	return minute
 }
 
 /**
