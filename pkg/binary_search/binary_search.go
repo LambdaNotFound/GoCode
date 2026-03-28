34f0dd6b24250a81f23d0fcdfead0adc57d568fd
LambdaNotFound <yiqyudev@gmail.com>
2026-03-27 18:21:54 -0700

    74. Search a 2D Matrix
    row, col := mid/n, mid%n

--- pkg/binary_search/binary_search.go
+++ pkg/binary_search/binary_search.go
@@ -207,3 +207,23 @@ func binarySearch(nums []int, target int) int {
 	}
 	return -1
 }
+
+/**
+ * 74. Search a 2D Matrix
+ */
+func searchMatrix(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	left, right := 0, m*n
+	for left < right {
+		mid := left + (right-left)/2
+		row, col := mid/n, mid%n
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return false
+}
