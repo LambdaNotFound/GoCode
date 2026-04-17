6feff561804120cca19ba0c7d64a5dd0516f8351
LambdaNotFound <yiqyudev@gmail.com>
2026-04-17 01:58:11 -0700

    295. Find Median from Data Stream
    w/ delete()

--- pkg/heap/median_finder.go
+++ pkg/heap/median_finder.go
@@ -2,6 +2,9 @@ package heap
 
 import "container/heap"
 
+/**
+ * 295. Find Median from Data Stream
+ */
 type MedianFinder struct {
 	largeHeap *Heap // MinHeap, intended to hold the larger half
 	smallHeap *Heap // MaxHeap, intended to hold the smaller half
@@ -40,3 +43,53 @@ func (this *MedianFinder) FindMedian() float64 {
 	}
 	return float64(this.smallHeap.Peek())
 }
+
+/** to support delete number
+
+type MedianFinder struct {
+    maxHeap  *Heap[int]
+    minHeap  *Heap[int]
+    deleted  map[int]int  // num → count of pending deletions
+    maxCount int          // effective size of maxHeap
+    minCount int          // effective size of minHeap
+}
+
+func (mf *MedianFinder) Delete(num int) {
+    mf.deleted[num]++
+
+    // determine which heap it logically belongs to and decrement count
+    if num <= mf.maxHeap.items[0] {
+        mf.maxCount--
+    } else {
+        mf.minCount--
+    }
+
+    mf.rebalance()
+}
+
+// prune removes logically deleted tops from heaps
+func (mf *MedianFinder) prune(h *Heap[int]) {
+    for h.Len() > 0 && mf.deleted[h.items[0]] > 0 {
+        mf.deleted[h.items[0]]--
+        heap.Pop(h)
+    }
+}
+
+func (mf *MedianFinder) rebalance() {
+    // prune stale tops before rebalancing
+    mf.prune(mf.maxHeap)
+    mf.prune(mf.minHeap)
+
+    if mf.maxCount > mf.minCount+1 {
+        mf.minCount++
+        mf.maxCount--
+        heap.Push(mf.minHeap, heap.Pop(mf.maxHeap))
+        mf.prune(mf.maxHeap)
+    } else if mf.minCount > mf.maxCount {
+        mf.maxCount++
+        mf.minCount--
+        heap.Push(mf.maxHeap, heap.Pop(mf.minHeap))
+        mf.prune(mf.minHeap)
+    }
+}
+*/
